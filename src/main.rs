046052b85bed6d6c71be36666215b5b583d8cf1c
Frederic Gos <frederic.gos@gmail.com>
2024-12-03 18:50:08 +0100

    Implement sorting algorithms with trait and example sorters. M;ade skeletons for QuickSort and MergeSort.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod algorithm;
+mod quicksort;
+mod mergesort;
+
+use algorithm::SortingAlgorithm;
+use quicksort::QuickSort;
+use mergesort::MergeSort;
+
 fn main() {
-    println!("Hello, world!");
+    let sorting_algorithms: Vec<Box<dyn SortingAlgorithm<Input = Vec<i32>, Output = Vec<i32>>>> = vec![Box::new(QuickSort), Box::new(MergeSort)];
+    sorting_algorithms.iter().for_each(|sorting_algorithm| {
+        let input = vec![3, 2, 1];
+        let output = sorting_algorithm.run(input);
+        println!("{:?}", output);
+    });
 }
